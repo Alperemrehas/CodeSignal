d4b7ba553b491fb6888c6d88a5c7d4870eed3ba9
Alemha <37380232+Alperemrehas@users.noreply.github.com>
2023-07-28 11:37:45 +0300

    Solved

--- alphabeticShift.py
+++ alphabeticShift.py
@@ -1,1 +1,17 @@
-print("Test")
+def solution(inputString):
+    new_string=''
+    for i in range(len(inputString)):        
+        ca = chr(ord(inputString[i])+1)
+        if ca > 'z':
+            new_string=new_string+'a'
+        else:    
+            new_string=new_string + ca
+    return new_string
+'''
+Given a string, your task is to replace each of its characters by the next one in the English alphabet; i.e. replace a with b, replace b with c, etc (z would be replaced by a).
+
+Example
+
+For inputString = "crazy", the output should be solution(inputString) = "dsbaz".
+'''
+
