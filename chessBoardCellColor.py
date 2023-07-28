6668aa843c8851ad6db85f8815fe38b9e3035743
Alemha <37380232+Alperemrehas@users.noreply.github.com>
2023-07-28 12:24:28 +0300

    Corrected

--- chessBoardCellColor.py
+++ chessBoardCellColor.py
@@ -13,19 +13,12 @@ def solution(cell1, cell2):
     return cell1_color == cell2_color
 
 
-'''def solution(cell1, cell2):
-    charachter_black = ['A', 'C', 'E', 'G']
-    charachter_white = ['B', 'D', 'F', 'H']
+'''Given two cells on the standard chess board, determine whether they have the same color or not.
 
-    cell1_c = cell1[0]
-    cell1_n = int(cell1[1])
-    cell2_c = cell2[0]
-    cell2_n = int(cell2[1])
+Example
 
-    cell1_color = 'b' if (cell1_c in charachter_black and cell1_n % 2 != 0) or (cell1_c in charachter_white and cell1_n % 2 == 0) else 'w'
-    cell2_color = 'b' if (cell2_c in charachter_black and cell2_n % 2 != 0) or (cell2_c in charachter_white and cell2_n % 2 == 0) else 'w'
-
-    return cell1_color == cell2_color
+For cell1 = "A1" and cell2 = "C3", the output should be
+solution(cell1, cell2) = true
 
 
 '''
